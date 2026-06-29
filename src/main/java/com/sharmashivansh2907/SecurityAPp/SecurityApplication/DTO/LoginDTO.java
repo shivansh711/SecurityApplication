e3b41b78d96ffff9a96450fd5b9d54c63a4d8d6f
Shivansh Sharma <shivanshsharma0711@gmail.com>
2026-06-30 03:40:59 +0530

    Security App

--- src/main/java/com/sharmashivansh2907/SecurityAPp/SecurityApplication/DTO/LoginDTO.java
+++ src/main/java/com/sharmashivansh2907/SecurityAPp/SecurityApplication/DTO/LoginDTO.java
@@ -4,7 +4,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class LoginDTO {
     @NotBlank(message = "Email of the user can not be blank")
     @Email(message = "Email should be valid")
